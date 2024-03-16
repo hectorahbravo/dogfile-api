bf4b90f8269b2bcf418d343160c24d6b5d01beef
Hector Hernandez <hectorahbravo@gmail.com>
2024-03-16 10:26:44 +0100

    fixes nodemailer

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,12 +1,12 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 // Create a transporter
 module.exports.transporter = nodemailer.createTransport({
-  service: 'gmail', // Use your preferred email service
+  service: "gmail", // Use your preferred email service
   auth: {
     user: process.env.NODEMAILER_EMAIL, // Your email
-    pass: process.env.NODEMAILER_PASSWORD // Your email account password or app-specific password
-  }
+    pass: process.env.NODEMAILER_PASSWORD, // Your email account password or app-specific password
+  },
 });
 
 // Create email template
@@ -16,7 +16,7 @@ module.exports.createEmailTemplate = (user) => {
       <h1>Valida tu email</h1>
       <p>Hola ${user.username} 👋🏻, ¡Gracias por registrarte en <strong>planesmadrid</strong>!</p>
       <p>Haz clic aquí para verificar tu email</p>
-      <a href="http://localhost:5173/activate/${user.activationToken}" style="background-color: green;color: whitesmoke;padding: 8px 12px;border-radius: 4px;text-decoration: none;">
+      <a href="https://dogfile.netlify.app/activate/${user.activationToken}" style="background-color: green;color: whitesmoke;padding: 8px 12px;border-radius: 4px;text-decoration: none;">
         Activar cuenta
       </a>
     </div>
